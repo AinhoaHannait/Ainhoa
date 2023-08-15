5407cb746276cce4635de1fa5fbd7e5bc23ad72c
AinhoaHannait <142331514+AinhoaHannait@users.noreply.github.com>
2023-08-15 10:19:42 -0600

    Add files via upload

---
+++ 14082023 Generaciones de las computadoras Ainhoa.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main (int argc,char **argv)
+{
+	system("color 1f");
+	setlocale(LC_ALL, "spanish");
+	
+	printf("Las generarciones de las computadoras y sus caracteristicas\n"
+	"\n"
+	"\tPrimera Generación 1940-1956\n"
+    "-Tubos de Vacío: Utilizaban tubos de vacío como principales componentes electrónicos\n"
+	"para el procesamiento y el almacenamiento de datos.\n"
+	"-Tamaño Enorme: Eran máquinas voluminosas que ocupaban habitaciones\n"
+	"enteras debido al tamaño de los componentes electrónicos.\n"
+	"-Programación Basada en Hardware: La programación se realizaba \n"
+	"de manera física mediante conexiones y configuraciones de interruptores y cables.\n"
+	"\n"
+	"\tSegunda Generación 1956-1963:\n"
+	"-Transistores: Reemplazaron los tubos de vacío,\n"
+	"lo que resultó en un aumento de velocidad y una reducción en el tamaño y consumo de energía.\n"
+	"-Sistemas de Cinta y Disco: Se introdujeron sistemas de almacenamiento en cinta y disco magnético,\n"
+	"mejorando el acceso a los datos. \n"
+	"-Lenguajes de Programación de Alto Nivel: Surgieron los primeros lenguajes de programación de \n"
+	"alto nivel, como el COBOL y el FORTRAN. \n"
+	"\n"
+	"\tTercera Generación 1964-1971:\n"
+	"-Circuitos Integrados: Se desarrollaron circuitos integrados (chips) que contenían múltiples transistores,\n"
+	"permitiendo una mayor densidad de componentes. \n"
+	"-Minicomputadoras: Aparecieron las minicomputadoras, más pequeñas y asequibles que sus predecesoras,\n"
+	"pero aún con potencia de cálculo razonable. \n"
+	"-Multiprogramación: Se introdujo la multiprogramación,\n"
+	"que permitía a las computadoras realizar varias tareas simultáneamente.\n"
+	"\n"
+	"\tCuarta Generación 1970-1980\n"
+	"-Microprocesadores: Los microprocesadores fueron introducidos, lo que permitió tener una\n"
+	"computadora completa en un solo chip. Esto impulsó la creación de computadoras\n"
+	"personales y contribuyó al aumento de la potencia de cálculo a nivel individual.\n"
+	"-Computadoras personales: La popularidad de las computadoras personales creció\n"
+	"enormemente durante esta generación, con máquinas como la IBM PC y la Apple\n"
+	"Macintosh. Las computadoras se volvieron más accesibles para un público más amplio.\n"
+	"-Interfaces gráficas y sistemas operativos amigables: Surgieron las interfaces gráficas de\n"
+	"usuario (GUI) y los sistemas operativos amigables, como el Apple Macintosh System y el\n"
+	"sistema operativo Windows. Estos avances hicieron que las computadoras fueran más fáciles de usar para usuarios no técnicos.\n"
+	"\n"
+	"\tQuinta Generación 1980-presente\n"
+	"-Computación paralela y supercomputadoras: Se enfocó en la computación paralela, que\n"
+	"involucra la ejecución simultánea de múltiples tareas para mejorar el rendimiento.\n"
+	"Surgieron supercomputadoras que podían realizar cálculos extremadamente complejos y de gran escala.\n"
+	"-Redes y comunicación global: La quinta generación vio la expansión de las redes de computadoras y la creación de Internet,\n"
+	"lo que permitió la comunicación y el intercambio de información a nivel global.\n"
+	"-Avances en inteligencia artificial: La quinta generación también es conocida por el desarrollo de la inteligencia artificial\n"
+	"y el inicio de la investigación en aprendizaje automático y redes neuronales artificiales.\n");
+
+
+
+
+	
+	
+	return 0;
+}
+
+
