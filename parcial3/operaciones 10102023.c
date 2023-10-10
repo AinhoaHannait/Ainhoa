6ce1b03895f847d21a47583a4f2a0dc9acabfed1
AinhoaHannait <142331514+AinhoaHannait@users.noreply.github.com>
2023-10-10 11:13:24 -0600

    operaciones 10102023.c

--- parcial3/operaciones 10102023.c
+++ parcial3/operaciones 10102023.c
@@ -4,15 +4,15 @@
 
 int main ()
 {
-	int n, i, j, k, resp = 0, ope, tem, suma, resultado;
+	int n, i, j, k, resp = 0, ope, temp, suma=0;
 	
 	srand(time(NULL));
 	
 	printf ("Elige una operacion para hacer con matices\n"
-	"1=suma\t 2=resta\t 3=diagonal 4=multiplicación \n");
+	"1=suma\t 2=resta\t 3=diagonaL\t4 =multiplicacion \n");
 	scanf("%d", &resp);
 	
-	printf("ingrese la longitud de la matriz (para suma, resta, diagonal)\n");
+	printf("ingrese la longitud de la matriz \n");
 	scanf("%d", &n);
 	int A[n][n], B[n][n], C[n][n];
 	
@@ -103,12 +103,50 @@ int main ()
 	}
 	printf ("La suma de su diagonal es: %d\n", suma);
 	}
-
+	
+	if(resp==4)
+	{
+			printf("Matriz A\n");
+			for (i=0; i<n; i++)
+			{
+				for(j=0; j<n; j++)
+				{
+					A[i][j] = rand () % 10 +1;
+					printf("%d ", A[i][j]);
+				}
+				printf("\n");
+			}
+			printf("Matriz B\n");
+			for (i=0; i<n; i++)
+			{
+				for(j=0; j<n; j++)
+				{
+					B[i][j] = rand () % 10 +1;
+					printf("%d ", B[i][j]);
+				}
+				printf("\n");
+			}
+			printf("Matriz C\n");
+			for (i=0; i<n; i++)
+			{
+				for (j=0; j<n; j++)
+				{
+					temp = 0;
+					for (k =0; k<n; k++)
+					{
+						temp += A[i][k] * B[k][j];
+					}
+					C[i][j] = temp;
+					printf("%d ", C[i][j]);
+				}
+				printf("\n");
+			}
 		
-	
-	
+		
+	}
 	
 	return 0;
+
 }
 
-
+	
