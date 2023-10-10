e7a442a9b4532c2137a0e0cfce85cc4dac74fb69
AinhoaHannait <142331514+AinhoaHannait@users.noreply.github.com>
2023-10-10 10:56:41 -0600

    Add files via upload

---
+++ parcial3/operaciones 10102023.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main ()
+{
+	int n, i, j, k, resp = 0, ope, tem, suma, resultado;
+	
+	srand(time(NULL));
+	
+	printf ("Elige una operacion para hacer con matices\n"
+	"1=suma\t 2=resta\t 3=diagonal 4=multiplicación \n");
+	scanf("%d", &resp);
+	
+	printf("ingrese la longitud de la matriz (para suma, resta, diagonal)\n");
+	scanf("%d", &n);
+	int A[n][n], B[n][n], C[n][n];
+	
+	switch (resp)
+	{
+		case 1:
+			printf("Matriz A\n");
+			for (i=0; i<n; i++)
+			{
+				for(j=0; j<n; j++)
+				{
+					A[i][j] = rand () % 10 +1;
+					printf("%d ", A[i][j]);
+				}
+				printf("\n");
+			}
+			printf("Matriz B\n");
+			for (i=0; i<n; i++)
+			{
+				for(j=0; j<n; j++)
+				{
+					B[i][j] = rand () % 10 +1;
+					printf("%d ", B[i][j]);
+				}
+				printf("\n");
+			}
+			printf("Matriz C\n");
+			for (i=0; i<n; i++)
+			{
+				for(j=0; j<n; j++)
+				{
+					C[i][j]= A[i][j] + B[i][j];
+					printf("%d ", C[i][j]);
+				}
+				printf("\n");
+			}
+			break;
+	case 2:
+			printf("Matriz A\n");
+			for (i=0; i<n; i++)
+			{
+				for(j=0; j<n; j++)
+				{
+					A[i][j] = rand () % 10 +1;
+					printf("%d ", A[i][j]);
+				}
+				printf("\n");
+			}
+			printf("Matriz B\n");
+			for (i=0; i<n; i++)
+			{
+				for(j=0; j<n; j++)
+				{
+					B[i][j] = rand () % 10 +1;
+					printf("%d ", B[i][j]);
+				}
+				printf("\n");
+			}
+			printf("Matriz C\n");
+			for (i=0; i<n; i++)
+			{
+				for(j=0; j<n; j++)
+				{
+					C[i][j]= A[i][j] - B[i][j];
+					printf("%d ", C[i][j]);
+				}
+				printf("\n");
+			}
+			break;
+	case 3:
+			printf("Matriz A\n");
+			for (i=0; i<n; i++)
+			{
+				for(j=0; j<n; j++)
+				{
+					A[i][j] = rand () % 10 +1;
+					printf("%d ", A[i][j]);
+				}
+				printf("\n");
+			}
+
+	for(i=0 ; i<n ; i++)
+	{
+		j=i;
+		printf("a[%d][%d] = %d\n", i, j, A[i][j]);
+		suma+= A[i][j];
+		
+	}
+	printf ("La suma de su diagonal es: %d\n", suma);
+	}
+
+		
+	
+	
+	
+	return 0;
+}
+
+
